1332ccd6eeaa422bfec067f9f70418db72999260
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-04-16 23:01:19 -0700

    add new html & css styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,11 @@
     }
   });
 
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#sideNav'
